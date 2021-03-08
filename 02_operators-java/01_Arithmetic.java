26c30086c9f1a202c485d0355526e4096dfccd4e
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-03-08 18:02:51 +0600

    reorganize folders and files

---
+++ 02_operators-java/01_Arithmetic.java
@@ -0,0 +1,28 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        // Arithmetic Operators
+        int a = 6;
+        int b = 3;
+
+        // 1. Addition operator (+)
+        System.out.println("a + b = " + (a + b));
+
+        // 2. Substraction operator (-)
+        System.out.println("a - b = " + (a - b));
+
+        // 3. Multiplication operator (*)
+        System.out.println("a * b = " + (a * b));
+
+        // 4. Division operator (/)
+        System.out.println("a / b = " + (a / b));
+
+        // 5. Modulo operator (Reminder after division)
+        System.out.println("a % b = " + (a % b));
+
+        // * Division operator in java
+        System.out.println(9 / 2);
+        System.out.println(9 / 2.0);
+        System.out.println(9.0 / 2);
+        System.out.println(9.0 / 2.0);
+    }
+}
