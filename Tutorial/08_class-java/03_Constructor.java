4ac1d6598165ff60fba71b92ae180df00840c958
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-03-13 22:10:41 +0600

    fix some bugs

---
+++ Tutorial/08_class-java/03_Constructor.java
@@ -0,0 +1,21 @@
+public class Constructor {
+	 private String name;
+
+	 // no args constructor
+//	 Constructor() {
+//	 	 // this is constructor
+//		  System.out.println("Constructor is called");
+//		  name = "Fahim Faisaal";
+//	 }
+
+	 // args constructor
+	 Constructor(String myName) {
+	 	 name = myName;
+	 	 System.out.println("My name is " + myName);
+	 }
+
+	 public static void main(String[] args) {
+	 	 Constructor obj = new Constructor("Fahim Faisaal");
+	 	 System.out.println("I am " + obj.name);
+	 }
+}
