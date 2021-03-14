2b26d6b6b2e59dd5cb2849397266706fcab0d7fd
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-03-15 00:33:07 +0600

    add some String method

--- src/String/Method.java
+++ src/String/Method.java
@@ -1,4 +1,5 @@
 package String;
+import java.util.*;
 
 public class Method {
 	public static void main(String[] args) {
@@ -11,9 +12,9 @@ public class Method {
 		 System.out.println("Length: " + length);
 
 		 /*
-		 * substring method
-		 * syntax of substring method -> string.substring(int startIndex, int endIndex)
-		 */
+		  * substring method
+		  * syntax of substring method -> string.substring(int startIndex, int endIndex)
+		  */
 		 String mainStr = "Hello, I am Fahim and i am 22";
 		 String subStr = mainStr.substring(7, 17);
 
@@ -21,9 +22,9 @@ public class Method {
 		 System.out.println("Sub String: " + subStr);
 
 		 /*
-		 * replace method
-		 * syntax of replace method -> string.replace(char oldChar, char newChar)
-		 */
+		  * replace method
+		  * syntax of replace method -> string.replace(char oldChar, char newChar)
+		  */
 		String palindrome = "madam";
 		System.out.println("madam to ladal -> " + palindrome.replace('m', 'l'));
 
@@ -39,5 +40,31 @@ public class Method {
 		  */
 		 String addDash = "API(Application programming Interface)";
 		 System.out.println(addDash.replaceAll(" ", "-"));
+
+		 /*
+		  * split method
+		  * syntax of split method -> string.split(String regex, int limit)
+		  */
+		 String str = "Hello, World!";
+		 String[] splitStr = str.split("", 5);
+		 System.out.println("String to Array -> " + Arrays.toString(splitStr));
+
+		 // now we can access the char by calling index
+		 System.out.println("on 0 index = " + splitStr[0]);
+		 System.out.println("on last index = " + splitStr[splitStr.length - 1]);
+
+		 /*
+		  * toLowerCase method
+		  * syntax of toLowercase method -> string.toLowerCase()
+		  */
+		 String toLower = "HELLO, I AM FAHIM FAISAL";
+		 System.out.println(toLower.toLowerCase());
+
+		 /*
+		  * toUpperCase method
+		  * syntax of toUpperCase method -> string.toUpperCase()
+		  */
+		 String toUpper = "hello, i am fahim faisal";
+		 System.out.println(toUpper.toUpperCase());
 	}
 }
