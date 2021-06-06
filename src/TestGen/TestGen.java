5deffd03b994cc45e6ba3aaa35206c174d4dae47
fahimfaisaal <fahimfaisal.soikot@gmail.com>
2021-06-06 08:32:57 +0600

    feat: add test generics

---
+++ src/TestGen/TestGen.java
@@ -0,0 +1,17 @@
+package TestGen;
+
+public class TestGen {
+	public String message;
+
+	public TestGen(String msg) {
+		this.message = msg;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String msg) {
+		this.message = msg;
+	}
+}
